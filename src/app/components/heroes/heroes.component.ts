e2d2d325327a5fc194f3461242a3f069af552654
Manu <manu.villar.vitoria@gmail.com>
2021-01-25 01:04:20 +0100

    Heroes Page - Design with * ngFor

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from "../../servicios/heroes.service";
 
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
